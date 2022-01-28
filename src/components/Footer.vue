0a82a65e1232c03d7ac0b093d0f229cd981baf74
Khalil_Audeh <khalilaudeh361997@gmail.com>
2022-01-28 18:15:44 +0200

    small update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,8 @@
     <div class="row">
       <div class="col py-4 text-center">
         <h6 class="m-0">
-          &copy; Copyright <span class="colored-bracket h4 font-weight-bold">[</span> fill in the
+          &copy; Copyright
+          <span class="colored-bracket h4 font-weight-bold">[</span> fill in the
           blank
           <span class="colored-bracket h4 font-weight-bold">]</span>
           , 2022.
@@ -32,6 +33,12 @@ export default {
   font-family: Georgia, serif;
   color: white;
   background-color: maroon;
-  letter-spacing: .2rem;
+  letter-spacing: 0.1rem;
+}
+
+@media all and (min-width: 992px) {
+  .footer {
+    letter-spacing: 0.2rem;
+  }
 }
 </style>
